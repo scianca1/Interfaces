9f528b18d1ac7e48ea043f404115ccb691d32588
Emilio Colombo <emiliocolombo97@gmail.com>
2023-10-01 20:54:30 -0300

    se coninuaa

---
+++ tp2/js/games.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const highlightsWeek = [
+    {
+        id : 1,
+        name : 'fifa 23',
+        image : '../imagenes/fifa23.webp',
+        button : "Jugar",
+        price : 0
+    },
+    {
+        id : 2,
+        name : 'Pokemon',
+        image : '../imagenes/god-of-war.jpg',
+        button : "Jugar",
+        price : 0
+    },
+    {
+        id : 3,
+        name : 'Uno',
+        image : '../imagenes/fifa23.webp',
+        button : "Jugar",
+        price : 0
+    },
+    {
+        id : 4,
+        name : 'Free fire',
+        image : '../imagenes/god-of-war.jpg',
+        button : "Jugar",
+        price : 0
+    },
+    {
+        id : 5,
+        name : 'Mario Bros',
+        image : '../imagenes/fifa23.webp',
+        button : "Jugar",
+        price : 0
+    },
+    {
+        id : 6,
+        name : 'Conecta 4',
+        image : '../imagenes/god-of-war.jpg',
+        button : "Jugar",
+        price : 0
+    },
+]
--- tp2/js/index.js
+++ tp2/js/index.js
@@ -1,5 +1,39 @@
-"use strict"
+"use strict";
 
 document.addEventListener("DOMContentLoaded",()=>{
     console.log("holi");
+
+
+
+    const highlightsWeekCarousel = document.getElementById('carousel');
+    const prevButton = document.getElementById('prevButton');
+    const nextButton = document.getElementById('nextButton');
+
+    highlightsWeek.forEach((gameWeek) => {
+        console.log(gameWeek.name);
+        let article = document.createElement("article");
+        article.className = "gameWeek";
+        article.innerHTML = `
+            <div class="card-mis-juegos">
+                <div class="gradiente">
+                    <button class="botonPrimario boton-Mediano">
+                        <div class="textoBotones texto-pequenio">
+                            jugar
+                        </div>
+                    </button>
+                </div>
+                <img class="imagen-card-mis-juegos" src="${gameWeek.image}" alt="">
+            </div>`;
+        
+        highlightsWeekCarousel.append(article);
+    });
+
+    prevButton.addEventListener("click", () => {
+        highlightsWeekCarousel.scrollLeft -= 1000;
+    });
+    
+    nextButton.addEventListener("click", () => {
+        highlightsWeekCarousel.scrollLeft += 1000;
+    });
+
 })
