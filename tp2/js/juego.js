13ddfbad1822d8054bbbd4fd1aa68f213f0a2fb2
Emilio Colombo <emiliocolombo97@gmail.com>
2023-10-09 13:28:35 -0300

    ARREGLE CARRUSEL JUEGO

--- tp2/js/juego.js
+++ tp2/js/juego.js
@@ -1,27 +1,11 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded",()=>{
-  // const iconoHamburguesa= document.getElementById('iconoHamburguesa');
-  // const menu_Hamburguesa=document.getElementById('menu_Hamburguesa');
-  
-  // iconoHamburguesa.addEventListener('click',(event)=>{
-  //  if(menu_Hamburguesa.classList.contains("menuHamburguesaCerrado")) {
-  //     menu_Hamburguesa.classList.remove("menuHamburguesaCerrado");
-  //  }else{
-  //   menu_Hamburguesa.classList.add("menuHamburguesaCerrado");
-  //  }
-  // });
-  //lo inclui en el home, despues hay que hacer 
-  //un js para el header, e incluirlo en todos los html
-
-
   // prueba
 
   const commonsAccionCarousel = document.getElementById('carrusel-comunes-accion');
   const nextButtons = document.querySelectorAll(".gameWeek-btn-next");
   const backButtons = document.querySelectorAll(".gameWeek-btn-back");
-  const nextButtonSlider = document.getElementById("btn-next-slider");
-  const backButtonSlider = document.getElementById("btn-back-slider")
   
   commons.forEach((game) => {
     let article = document.createElement("article");
@@ -48,6 +32,7 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 backButtons.forEach((button) => {
   button.addEventListener("click", () => {
+      console.log("back")
       const duracionAnimacion = 1000;
       const distanciaPorPaso = 10; // Distancia a desplazar en cada paso
       let tiempoInicio;
@@ -64,6 +49,27 @@ backButtons.forEach((button) => {
       tiempoInicio = null; // Reinicia el tiempo de inicio
       requestAnimationFrame(animarScroll(0));
   });
+
+})
+
+nextButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const duracionAnimacion = 1000;
+        const distanciaPorPaso = 10; // Distancia a desplazar en cada paso
+        let tiempoInicio;
+        function animarScroll(timestamp) {
+            if (!tiempoInicio) tiempoInicio = timestamp;
+            const tiempoTranscurrido = timestamp - tiempoInicio;
+            const progreso = Math.min(tiempoTranscurrido / duracionAnimacion, 1);
+            const distanciaPaso = distanciaPorPaso * progreso;
+            button.parentNode.parentNode.scrollLeft += distanciaPaso;
+            if (progreso < 1) {
+                requestAnimationFrame(animarScroll);
+            }
+        }
+        tiempoInicio = null; // Reinicia el tiempo de inicio
+        requestAnimationFrame(animarScroll(0));
+    });
 
 })
 
