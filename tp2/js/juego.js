768a14c18eab7a22340008f88d63733c978db5fc
Emilio Colombo <emiliocolombo97@gmail.com>
2023-10-09 00:52:27 -0300

    termine carrito y perfil

--- tp2/js/juego.js
+++ tp2/js/juego.js
@@ -1,15 +1,17 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded",()=>{
-  const iconoHamburguesa= document.getElementById('iconoHamburguesa');
-  const menu_Hamburguesa=document.getElementById('menu_Hamburguesa');
+  // const iconoHamburguesa= document.getElementById('iconoHamburguesa');
+  // const menu_Hamburguesa=document.getElementById('menu_Hamburguesa');
   
-  iconoHamburguesa.addEventListener('click',(event)=>{
-   if(menu_Hamburguesa.classList.contains("menuHamburguesaCerrado")) {
-      menu_Hamburguesa.classList.remove("menuHamburguesaCerrado");
-   }else{
-    menu_Hamburguesa.classList.add("menuHamburguesaCerrado");
-   }
-  });
+  // iconoHamburguesa.addEventListener('click',(event)=>{
+  //  if(menu_Hamburguesa.classList.contains("menuHamburguesaCerrado")) {
+  //     menu_Hamburguesa.classList.remove("menuHamburguesaCerrado");
+  //  }else{
+  //   menu_Hamburguesa.classList.add("menuHamburguesaCerrado");
+  //  }
+  // });
+  //lo inclui en el home, despues hay que hacer 
+  //un js para el header, e incluirlo en todos los html
 
 });
