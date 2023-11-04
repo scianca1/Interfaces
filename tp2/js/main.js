01114d9ed4ef32c32c1404343f1d109433f45c68
Emilio Colombo <emiliocolombo97@gmail.com>
2023-11-04 14:09:15 -0300

    agregue inicio y pantallas de ganador y empate

--- tp2/js/main.js
+++ tp2/js/main.js
@@ -3,19 +3,44 @@
 document.addEventListener("DOMContentLoaded",()=>{
     let canvas= document.getElementById('canvas');
     let ctx= canvas.getContext("2d");
-    // console.log(ctx);
     let canvasWidth= canvas.width;
     let canvasHeight=canvas.height;
     ctx.fillStyle= '#F8F8FF';
     ctx.fillRect(0,0,canvasWidth,canvasHeight);
-
-    // this.canvas.addEventListener("mousedown", (e)=>{
-    //     this.onMouseDown(e, this.tablero);
-    // }, false);
-
-    //let tablero = new Tablero(canvas, ctx, "red", 40, 20, 220, 130, 6, 7, 7);
-    let juego = new CuatroEnLinea(canvas, ctx, "red", 170, 70, 700, 350, 6, 7, 20, "../imagenes/canchafutbol.jpg", "../imagenes/Ficha_brazil.png", "../imagenes/fichaFrancia.png");
-    juego.iniciarJuego();
+    
+    let pantallaInicio = document.querySelector("#inicioJuego");
+    let contenidoPantallaInicio = htmlsPantallas[0];
+    //cambiando el 0 por el 1 = pantalla gandor, y el 0 por el 2, empate
+    if(contenidoPantallaInicio.nombre == "pantallaInicio"){
+        pantallaInicio.innerHTML = contenidoPantallaInicio.html;
+        let botonJugarJuego = document.querySelector("#botonJugarJuego");
+        botonJugarJuego.addEventListener("click", ()=>{
+            pantallaInicio.style.display = "none";
+            let filas;
+            let columnas;
+            let equipoUno;
+            let equipoDos;
+            console.log(canvas);
+            //cargar data, e iniciar el juego
+            let juego = new CuatroEnLinea(canvas, ctx, "red", 170, 70, 700, 350, 6, 7, 20, "../imagenes/canchafutbol.jpg", "../imagenes/Ficha_brazil.png", "../imagenes/fichaFrancia.png");
+            juego.iniciarJuego();
+        })
+    }else if(contenidoPantallaInicio.nombre == "pantallaGanador"){
+        let contenido = contenidoPantallaInicio.head;
+        contenido += "felicidades, ha ganado el jugador 1";
+        contenido += contenidoPantallaInicio.body;
+        pantallaInicio.innerHTML = contenido;
+        let botonVolveraJugar = document.querySelector("#botonVolveraJugar");
+        botonVolveraJugar.addEventListener("click", ()=>{
+            window.location.reload();
+        })
+    }else if(contenidoPantallaInicio.nombre == "pantallaEmpate"){
+        pantallaInicio.innerHTML = contenidoPantallaInicio.html;
+        let botonVolveraJugar = document.querySelector("#botonVolveraJugar");
+        botonVolveraJugar.addEventListener("click", ()=>{
+            window.location.reload();
+        })
+    }
    
 
     
---
+++ tp2/js/pantallasData.js
@@ -0,0 +1,97 @@
+htmlsPantallas = [
+    {
+        nombre : "pantallaInicio",
+        html : `
+        <form class="formTableros">
+            <h2 class="tituloTableros">elegir modalidad de juego</h2>
+            <div class="form-check-tablero">
+                <img class="imagenTablero" src="../imagenes/tablero6x7.png" alt="">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
+                <label class="form-check-label" for="flexRadioDefault1">
+                    4 en linea
+                </label>
+            </div>
+            <div class="form-check-tablero">
+                <img class="imagenTablero" src="../imagenes/tablero7x8.png" alt="">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                <label class="form-check-label" for="flexRadioDefault1">
+                    5 en linea
+                </label>
+            </div>
+            <div class="form-check-tablero">
+                <img class="imagenTablero" src="../imagenes/tablero8x9.png" alt="">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                <label class="form-check-label" for="flexRadioDefault2">
+                    6 en linea
+                </label>
+            </div>
+        </form>
+        <button class="botonPrimario  boton-Mediano botonJugarJuego" id="botonJugarJuego">jugar</button>
+        <div class="jugadores">
+            <h2 class="tituloJugador">jugador 1</h2>
+            <form class="formFichas">
+                <div class="form-check">
+                    <img class="imagenFicha" src="../imagenes/fichaFrancia.png" alt="">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        francia
+                    </label>
+                </div>
+                <div class="form-check">
+                    <img class="imagenFicha" src="../imagenes/Ficha_Argentina.png" alt="">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        argentina
+                    </label>
+                </div>
+                <div class="form-check">
+                    <img class="imagenFicha" src="../imagenes/Ficha_brazil.png" alt="">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                    <label class="form-check-label" for="flexRadioDefault2">
+                        brasil
+                    </label>
+                </div>
+            </form>
+            <h2 class="tituloJugador">jugador 2</h2>
+            <form class="formFichas">
+                <div class="form-check">
+                    <img class="imagenFicha" src="../imagenes/fichaFrancia.png" alt="">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        francia
+                    </label>
+                </div>
+                <div class="form-check">
+                    <img class="imagenFicha" src="../imagenes/Ficha_Argentina.png" alt="">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        argentina
+                    </label>
+                </div>
+                <div class="form-check">
+                    <img class="imagenFicha" src="../imagenes/Ficha_brazil.png" alt="">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                    <label class="form-check-label" for="flexRadioDefault2">
+                        brasil
+                    </label>
+                </div>
+            </form>
+        </div>`
+    },
+    {
+        nombre: "pantallaGanador",
+        head: `
+                <h1 class="textoGanador">`,
+        body:`</h1>
+                <button class="botonPrimario botonJugarJuego" id="botonVolveraJugar">volver a jugar</button>
+            `
+    },
+    {
+        nombre: "pantallaEmpate",
+        html: `
+                    <h1 class="textoGanador">es un empate!, puedes intentar de nuevo ;)</h1>
+                    <button class="botonPrimario botonJugarJuego" id="botonVolveraJugar">volver a jugar</button>
+                </div>
+                `
+    }
+]
