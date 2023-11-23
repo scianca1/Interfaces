b928cc9bcb8b93fe9397063d45337e4da1d5b10b
Matias Sciancalepore <msciancalepore47@gmail.com>
2023-11-23 19:26:57 -0300

    listo el loader y empezando el hover del spriteshet

--- tp4/js/mains.js
+++ tp4/js/mains.js
@@ -134,4 +134,39 @@ document.addEventListener("DOMContentLoaded",()=>{
         //     logo.style.top = '250px'; /* Restaura la posición vertical original del logo al volver arriba */
         // }
     });
+    const carga= document.getElementById('background');
+const body = document.getElementById('body');
+const percentage = document.getElementById('porsentaje');
+    body.style.overflow = 'hidden';
+    updatePercentage();
+ function updatePercentage() {
+        let width = 0;
+        let percent = 0;
+        
+        const interval = setInterval(() => {
+            width += 1;
+            percent += 1;
+    
+            if (width <= 100) {
+                // if(width >=60){
+                //     bar.style.width = width + "%";
+                // percentage.textContent = percent + "%"+" ya casii!";
+                // }else{
+                bar.style.width = width + "%";
+                percentage.textContent = percent + "%";
+                // }
+            } else {
+                clearInterval(interval);
+                closecarry();
+                body.style.overflow = 'auto';
+                body.style.overflowX='hidden'
+            }
+        }, 50);
+        
+    }
+function closecarry(){
+        carga.classList.add("desbanecer");
+        carga.style.display='none'; 
+        
+    }
 });
